de5074832db792947bc4e4e1c3ad5ff0ea84fa55
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-22 05:16:18 -0700

    Upd

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -19,7 +19,7 @@ class Task(models.Model):
 
     title = models.CharField(max_length=100, db_index=True)
     description = models.TextField()
-    assigned = models.ForeignKey(MyUser)
+    assigned = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=32,
         choices=STATUS,
