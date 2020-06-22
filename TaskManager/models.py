e50d98d29b5a6724212d500ee6639463fd15ca8c
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-22 05:09:29 -0700

    Update models.py

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -7,6 +7,9 @@ class MyUser(models.Model):
     login = models.CharField(max_length=20)
     passw = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return self.login
+
 
 class Task(models.Model):
     STATUS = {
