30faef8d1b4f90fc6477be6b1754421664cab05a
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-22 14:18:11 +0300

    Update views.py

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -2,9 +2,30 @@ from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from .models import Task
+from .serializers import TaskSerializer
 
-from TaskManager.models import Task
-from TaskManager.serializers import TaskSerializer
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+from .models import MyUser
+from .forms import RegisterForm
+
+def index(request):
+    return render(request, 'TaskMan/index.html')
+
+def register(request):
+    alert = False
+    if(request.method == 'POST'):
+        try:
+            form = RegisterForm(request.POST)
+            form.save()
+        except:
+            alert = True
+
+    context = {'form': form, 'alert': alert}
+
+    return render(request, 'TascMan/register.html', context)
 
 
 class TaskListView(GenericAPIView):
