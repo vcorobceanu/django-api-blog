840ef08811bc37a2b874e12bbdc21b850a56cb7d
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-22 14:40:04 +0300

    Register form

--- TaskManager/forms.py
+++ TaskManager/forms.py
@@ -4,4 +4,23 @@ from django.forms import ModelForm, TextInput
 class RegisterForm(ModelForm):
     class Meta:
         model = MyUser
-        fields = ['name', 'pren', 'login', 'passw']
+        # fields = ['name', 'pren', 'login', 'passw']
+        fields = '__all__'
+
+        widgets = {'name': TextInput(attrs={
+            'style': 'margin-top: 10px;',
+            'placeholder': 'Nume'
+        }),
+        'pren': TextInput(attrs={
+            'style': 'margin-top: 10px;',
+            'placeholder': 'Prenume'
+        }),
+        'login': TextInput(attrs={
+                'style': 'margin-top: 10px;',
+                'placeholder': 'Log in'
+            }),
+        'passw': TextInput(attrs={
+                'style': 'margin-top: 10px;',
+                'placeholder': 'Parola'
+            }),
+        }
