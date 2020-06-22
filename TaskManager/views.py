3e6a7af70169b6b9ec1954a8e8ddc5789ab2da36
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-22 15:19:59 +0300

    Update views.py

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -43,7 +43,7 @@ def login(request):
 
             if ok == True:
                 request.session['userpass'] = rez_user.passw
-                return redirect("/TaskManager")
+                return redirect("/TaskManager/list")
             else:
                 alert = True
 
