686f9f917a013d2ccc98ce284b4242b6df908126
ionmorarudj <64538297+ionmorarudj@users.noreply.github.com>
2020-06-22 14:36:15 +0300

    Updates

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('/register', views.register),
+    path('register', views.register),
+    path('list', views.list ,name = "list"),
     # path('/login', views.login),
     # path('/logout', views.logout),
 ]
