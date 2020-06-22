7dcad0898ce7a935204ee7c788c898599f4ad873
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-22 14:56:40 +0300

    view

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -9,12 +9,26 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import MyUser
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 def index(request):
     return render(request, 'TaskMan/index.html')
 
 def register(request):
+    alert = False
+    if(request.method == 'POST'):
+        try:
+            form = RegisterForm(request.POST)
+            form.save()
+        except:
+            alert = True
+    form = RegisterForm()
+    context = {'form': form, 'alert': alert}
+
+    return render(request, 'TaskMan/register.html', context)
+
+
+def login(request):
     alert = False
     if(request.method == 'POST'):
         try:
