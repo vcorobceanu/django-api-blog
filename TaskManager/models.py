2c2eed8457dbd32c715f7d83ef7cd7aaa91b2b1e
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-22 03:44:21 -0700

    Aded task model

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+    description = models.TextField()
+    assigned = models.CharField(max_length=30, db_index=True)
