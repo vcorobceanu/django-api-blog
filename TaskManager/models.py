ba4f2041461e616624331f304033a2b1b79d005f
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-22 13:45:22 +0300

    Modelul MyUser

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class MyUser(models.Model):
+    name = models.CharField(max_length=20)
+    pren = models.CharField(max_length=20)
+    login = models.CharField(max_length=20)
+    passw = models.CharField(max_length=20, unique=True)
