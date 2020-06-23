dc0a86d1b9f362913882e59cdbc38f169edc0cc4
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-23 09:19:05 +0300

    Update urls.py

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('register/', views.register),
-    path('list/', views.list),
-    path('newtask/', views.newtask, name="newtask"),
     path('login/', views.login_view),
     path('logout/', views.logout_view),
+    path('list/', views.list),
+    path('list/<str:title>/', views.taskitem),
+    path('newtask/', views.newtask, name = "newtask"),
     # path('mytasks/', views.mytasks),
 ]
+
 
