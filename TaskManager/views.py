2c5772e47b8a524f3026cd7587882d8342dbe238
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-23 09:59:02 +0300

    Update views.py

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -103,3 +103,9 @@ def mytasks(request):
     context = {'task': tasks}
     print(tasks)
     return render(request, 'TaskMan/list.html', context)
+
+
+def completed(request):
+    tasks = Task.objects.filter(status="open")
+    context = {'task': tasks}
+    return render(request, 'TaskMan/list.html', context)
