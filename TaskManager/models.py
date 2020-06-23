000ae43a54562f4d7b9084944c4012d7003a8204
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-23 11:08:52 +0300

    All

--- TaskManager/models.py
+++ TaskManager/models.py
@@ -21,3 +21,9 @@ class Comments(models.Model):
     text = models.CharField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+
+class Notification(models.Model):
+    assigned = models.ForeignKey(User, on_delete=models.CASCADE)
+    info = models.CharField(max_length=399)
+    seen = models.BooleanField(default=False)
