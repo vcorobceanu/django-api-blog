c2ef205a65689ca0b0b16afff8f43c4af136a7f9
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-23 09:15:49 +0300

    Stiling

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('register/', views.register),
-    path('list/', views.list),
-    path('newtask/', views.newtask, name = "newtask"),
     path('login/', views.login_view),
     path('logout/', views.logout_view),
+    path('list/', views.list),
+    path('newtask/', views.newtask, name = "newtask"),
     # path('mytasks/', views.mytasks),
 ]
+
+
