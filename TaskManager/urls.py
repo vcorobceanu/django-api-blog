de6975c478f5d02e481e551102b381f04a032652
ionmorarudj <64538297+ionmorarudj@users.noreply.github.com>
2020-06-23 10:01:32 +0300

    Tem

--- TaskManager/urls.py
+++ TaskManager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = "index"),
     path('register/', views.register),
     path('list/', views.list),
     path('newtask/', views.newtask, name = "newtask"),
