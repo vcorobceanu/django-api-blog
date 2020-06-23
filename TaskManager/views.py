b68b6aa887a0510e8b303093bc6d9e5388e33680
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-23 09:41:56 +0300

    Update views.py

--- TaskManager/views.py
+++ TaskManager/views.py
@@ -91,7 +91,6 @@ def newtask(request):
         return render(request, 'TaskMan/newtask.html', context)
 
 def taskitem(request, title):
-    print(title)
     task = Task.objects.get(title=title)
     context = {'task': task}
     return render(request,'TaskMan/task_info.html', context)
