ef16f962e95b826b251ea05cc264a13041c1b04b
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-25 11:43:40 +0300

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
 
@@ -8,15 +8,13 @@ from TaskManager.models import Task, Comment, Notification
 class ViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        # Set up non-modified objects used by all test methods
         user = User.objects.create(username='Billy', password='Milligan')
         user.save()
         task = Task.objects.create(title='Big', description='Bob', assigned=user, author=user)
-        task.save()
         comment = Comment.objects.create(text='text', author=user, task_id=1)
-        comment.save()
         notification = Notification.objects.create(assigned_id=1, info='Notification info', task_id=1)
-        notification.save()
+
+    pass
 
     def test_index(self):
         resp = self.client.get(reverse('index'))
@@ -24,4 +22,9 @@ class ViewTest(TestCase):
 
     def test_redirect_if_not_logged_in(self):
         resp = self.client.get(reverse('list'))
-        self.assertRedirects(resp, '/TaskManager/login/?next=/TaskManager/list/')
+        self.assertEquals(resp.status_code, 302)
+
+    def test_login_user(self):
+        response = self.client.post(reverse('login'), {'username': 'Billy', 'password': 'Milligan'})
+        self.assertEquals(response.status_code, 200)
+
