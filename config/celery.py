6b376e5705e9f4a3db754337f0d3b34158be741c
RodiZaharadji <65939263+RodiZaharadji@users.noreply.github.com>
2020-06-25 17:48:58 +0300

    Noooo

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('TaskManager')
+app = Celery('config')
 app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
--- config/settings.py
+++ config/settings.py
@@ -227,3 +227,9 @@ SIMPLE_JWT = {
 
 }
 
+# REDIS related settings
+REDIS_HOST = 'localhost'
+REDIS_PORT = '6379'
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
