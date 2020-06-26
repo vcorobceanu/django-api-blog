c18792b310b20f14e3de9b7e8c604ad9e9265faf
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-26 16:50:18 +0300

    upd

--- TaskManager/search_indexes.py
+++ TaskManager/search_indexes.py
@@ -23,47 +23,51 @@ html_strip = analyzer(
 class TaskDocument(Document):
     """Task Elasticsearch document."""
 
-    title = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            'raw': fields.TextField(analyzer='keyword'),
-        }
+
+title = fields.TextField(
+    analyzer=html_strip,
+    fields={
+        'raw': fields.TextField(analyzer='keyword'),
+    }
+)
+
+description = fields.TextField(
+    analyzer=html_strip,
+    fields={
+        'raw': fields.TextField(analyzer='keyword'),
+    }
+)
+
+author = fields.ObjectField(properties={
+    'username': fields.TextField(analyzer=html_strip)
+})
+
+assigned = fields.ObjectField(properties={
+    'username': fields.TextField(analyzer=html_strip)
+})
+
+status = fields.BooleanField(attr='status_indexing')
+is_started = fields.BooleanField(attr='is_started_indexing')
+
+
+class Django(object):
+    """Inner nested class Django."""
+
+    model = Task  # The model associate with this Document
+    related_models = [User]
+
+
+def get_queryset(self):
+    """Not mandatory but to improve performance we can select related in one sql request"""
+    return super(TaskDocument, self).get_queryset().select_related(
+        'User'
     )
 
-    description = fields.TextField(
-        analyzer=html_strip,
-        fields={
-            'raw': fields.TextField(analyzer='keyword'),
-        }
-    )
 
-    author = fields.ObjectField(properties={
-        'username': fields.TextField(analyzer=html_strip)
-    })
-
-    assigned = fields.ObjectField(properties={
-        'username': fields.TextField(analyzer=html_strip)
-    })
-
-    status = fields.BooleanField(attr='status_indexing')
-    is_started = fields.BooleanField(attr='is_started_indexing')
-
-    class Django(object):
-        """Inner nested class Django."""
-
-        model = Task  # The model associate with this Document
-        related_models = [User]
-
-    # def get_queryset(self):
-    #     """Not mandatory but to improve performance we can select related in one sql request"""
-    #     return super(TaskDocument, self).get_queryset().select_related(
-    #         'User'
-    #     )
-    #
-    # def get_instances_from_related(self, related_instance):
-    #     """If related_models is set, define how to retrieve the Car instance(s) from the related model.
-    #     The related_models option should be used with caution because it can lead in the index
-    #     to the updating of a lot of items.
-    #     """
-    #     if isinstance(related_instance, User):
-    #         return related_instance.Task
+def get_instances_from_related(self, related_instance):
+    """If related_models is set, define how to retrieve the Car instance(s) from the related model.
+    The related_models option should be used with caution because it can lead in the index
+    to the updating of a lot of items.
+    """
+    if isinstance(related_instance, User):
+        return related_instance.Task
