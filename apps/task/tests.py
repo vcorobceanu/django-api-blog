43563c789a3a0ca80ab24231000041bd1549e65b
ionmorarudj <64538297+ionmorarudj@users.noreply.github.com>
2020-06-26 15:17:30 +0300

    a

--- apps/task/tests.py
+++ apps/task/tests.py
@@ -15,8 +15,7 @@ class AuthentificationTestCase(APITestCase):
 
 class LogInTestCase(APITestCase):
 
-    @classmethod
-    def test_setUp(self):
+    def setUp(self):
         self.user = User.objects.create(username='Billy', password='Milligan')
         Token.objects.create(user=self.user)
         self.factory = APIRequestFactory()
@@ -24,3 +23,26 @@ class LogInTestCase(APITestCase):
         force_authenticate(self.request, user=self.user)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
+
+    def test_post_task(self):
+        self.client.post(reverse('post_task'), {"title": "413",
+                                                "description": "238",
+                                                "status": "open",
+                                                "is_started": "False",
+                                                "author": "1",
+                                                "assigned": "1"})
+
+    def test_post_comm(self):
+        self.client.post(reverse('post_comm'), {"text": "yardage",
+                                                "author": "1",
+                                                "task": "4"})
+
+    def test_get_task(self):
+        response = self.client.get(reverse('task_by_id', args=[4]))
+        self.assertEqual(response.data, {"assigned": "1",
+                                         "author": "1",
+                                         "description": "8",
+                                         "id": "4",
+                                         "is_started": "False",
+                                         "status": "closed",
+                                         "title": "4"})
--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,4 @@
-"""tribes_main URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # from django.contrib import admin
 from django.contrib import admin
 from django.urls import path, include
@@ -27,7 +13,7 @@ urlpatterns = [
     path('common/', include("apps.common.urls")),
     path('blog/', include("apps.blog.urls")),
     path('users/', include("apps.users.urls")),
-    path('task/', include("apps.task.urls"), name='task'),
+    path('task/', include("apps.task.urls")),
     path('TaskManager/', include("TaskManager.urls")),
     url(r'^search/', include(search_index_urls)),
 ]
