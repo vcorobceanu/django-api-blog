5d9eedfc2e7437890a13cfef1bc47b0a5a883c85
ionmorarudj <64538297+ionmorarudj@users.noreply.github.com>
2020-06-29 16:44:19 +0300

    Revert "Merge branch 'Development' of https://github.com/vcorobceanu/django-api-blog into Development"
    This reverts commit 3e5df4cae8604749bfd9c6ffc2bc8fad734aa559, reversing
    changes made to 19dd25c8852f4d52e4f4dff6d09331c12fd4a14a.

--- TaskManager/exports.py
+++ TaskManager/exports.py
@@ -1,7 +1,6 @@
 import csv
 import xlwt
-import time
-import os
+import json
 
 from celery import shared_task
 from django.contrib.auth.models import User
@@ -18,7 +17,7 @@ def in_csv(user_id):
     # response['Content-Disposition'] = 'attachment; filename="TaskList.csv"'
     with open("%s%s.csv" % ("exports/", in_csv.request.id), "w+") as f:
         writer = csv.writer(f)
-        time.sleep(2)
+
         writer.writerow(['Title', 'Description', 'Status', 'Comments count', 'Timelogs count', 'Total loget time'])
         for task in Task.objects.all():
             last_log = None
@@ -29,14 +28,16 @@ def in_csv(user_id):
             writer.writerow([task.title, task.description, task.status, task.comment_set.count(),
                              task.timelog_set.filter(user=user).count(), last_log])
 
-    print('este')
     # return str(response._container)
     return True
 
 
+
 @shared_task
 def from_excel(user_id):
+    print('start')
     user = User.objects.get(id=user_id)
+    print(user)
     # response = HttpResponse(content_type='application/ms-excel')
     # response['Content-Disposition'] = 'attachment; filename="TaskList.xls"'
 
@@ -55,17 +56,15 @@ def from_excel(user_id):
 
     font_style = xlwt.XFStyle()
 
+    print('este1')
     rows = Task.objects.all().values_list('title', 'description', 'status')
-
-    # for i in range(10):
-    #     print(1)
-    time.sleep(2)
 
     for row in rows:
         row_num += 1
         for col_num in range(len(row)):
             ws.write(row_num, col_num, row[col_num], font_style)
 
+    print('este2')
     row_num = 0
     col_num = 3
     for task in Task.objects.all():
@@ -78,16 +77,10 @@ def from_excel(user_id):
         ws.write(row_num, col_num, task.comment_set.count())
         ws.write(row_num, col_num + 1, task.timelog_set.filter(user=user).count())
         ws.write(row_num, col_num + 2, str(last_log))
+        print('este3')
 
     wb.save("%s%s.xls" % ("exports/", from_excel.request.id))
-    print('este')
+
+    print('este4')
     return True
 
-
-@shared_task
-def clear_exports():
-    file_dir = 'exports/'
-    for path in os.listdir(file_dir):
-        full_path = os.path.join(file_dir, path)
-        if os.path.isfile(full_path):
-            os.remove(full_path)
