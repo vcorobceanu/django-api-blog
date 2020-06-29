49563d03cacd1ee7a3e64c63c16148ecb9c894ca
Nichita Lozovan <49370707+ProNickOwO@users.noreply.github.com>
2020-06-29 16:43:24 +0300

    upd

--- TaskManager/elastic.py
+++ TaskManager/elastic.py
@@ -22,7 +22,7 @@ headers = {'Authorization': 'Token ' + token}
 
 # es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 query = es.search(index="search",
-                  body={'query': {'fuzzy': {'title': 'tas'.lower}}})['hits']
+                  body={'query': {'regexp': {'title': 'a'}}})['hits']
 sub = query['hits']
 print(sub)
 source = []
